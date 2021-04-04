f8c649851891155f2bca6631b18ce46f336d60f5
ZoomRmc <zoomrmc@gmail.com>
2021-04-04 06:13:42 +0300

    Avoid string copying trick, refactor hot loop
    @alaviss offered a trick which avoids superfluous copying of a string
    on slicing the buffer and table insertion. This alone boosted the speed
    by ~20%. Standard library tables need to take advantage of Nim's
    move semantics.
    
    The main loop was rewritten to use a basic FSM and a few buggy hectic
    lines were put in order so everything is a bit simpler and shorter.
    Unfortunately, only negligible speed gains (<5%) on this front.

--- README.md
+++ README.md
@@ -5,18 +5,22 @@ See the full original article for context and background: [https://benhoyt.com/w
 Original repository [benhoyt/countwords](https://github.com/benhoyt/countwords) is closed for further contributions.
 
 ## Status:
-Currently about 25-30% slower than Go.
+Mission accomplished: Currently about 1-3% faster than Go.
+
+Code tries to stay close to the same basic algorithm used by most optimized versions from the original repo.
 
 Probably no point running with `--gc:` other than `arc` or `orc`. Last comparison of various included garbage collectors:
 ```
-arc: 7.1s
-refc: 9.2s
-markAndSweep: 10.7s
-regions: 13.1s
-boehm: 21.2s
+arc/orc: 5.6s
+boehm: 5.7s
+markAndSweep: 5.9s
+regions: 6.1s
+refc: 6.4s
 ```
 
 ## Timing the current implementation
+To get statistically significant result measure multiple times or use specialized tools like [hyperfine](https://github.com/sharkdp/hyperfine)
+
 Required:
 - Bash
 - Go
--- optimized.nim
+++ optimized.nim
@@ -1,11 +1,31 @@
-from tables import initCountTable, inc, sort, pairs
-from algorithm import SortOrder
+from tables import initCountTable, inc, sort, pairs, len
+from algorithm import SortOrder, sortedByIt, sort
 from hashes import Hash, `!$`, `!&`
 
 proc hash*(x: string): Hash {.inline.} =
+  # FNV-1a 64
+  var h = 0x100000001b3'u64
   for c in x:
-    result = result !& cast[Hash](ord(c))
-  result = !$result
+    h = h xor cast[uint64](c)
+    h *= 0x84222325'u64
+  return cast[Hash](h)
+
+# Workaround for superfluous copying on table insertion by @alaviss
+template `^^`(s, i: untyped): untyped =
+  (when i is BackwardsIndex: s.len - int(i) else: int(i))
+
+template `{}`[T, U](s: string, slice: HSlice[T, U]): untyped =
+  s.toOpenArray(s ^^ slice.a, s ^^ slice.b)
+
+func copy(s: var string, data: openArray[char]) {.inline.} =
+  s.setLen(data.len())
+  for idx, c in data:
+    s[idx] = c
+# end of workaround
+
+template isWhitespace(c: char): untyped = (c <= ' ')
+
+template notWhitespace(c: char): untyped = (c > ' ')
 
 proc main() =
   const bufLen = 64 * 1024
@@ -13,50 +33,47 @@ proc main() =
     words = initCountTable[string]()
     buf = newString(bufLen)
     offset = 0
+    wordBuf: string
 
   while true:
     # Fill buffer starting from offset (remainder from prev. iteration)
-    let bytesRead = stdin.readBuffer(addr(buf[offset]), bufLen - offset)
-    if bytesRead == 0:
+    let totalBytes = offset + stdin.readChars(buf{offset .. ^1})
+    if totalBytes == 0:
       break
-    
-    let totalBytes = offset + bytesRead
     # Scan the buffer for the last word bound
     # to process only up to the potential partial at the end 
     var lastChar = totalBytes - 1 # an index, never < 0
-    while lastChar >= 0:
-      if buf[lastChar] <= ' ':
-        break
+    while lastChar > 0 and buf[lastChar].notWhitespace():
       lastChar.dec()
+    offset = totalBytes - lastChar - 1
 
-    # Whole buffer is a word/whitespace edgecase. #DELETEME or not?
-    if lastChar < 0:
-      lastChar = totalBytes - 1
-    
     # Split buffer into words and process
-    var wstart = 0
-    while wstart < lastChar:
-      # Skip leading whitespace
-      while buf[wstart] <= ' ':
-        wstart.inc()
-      var wend = wstart
-      # Iterate and lowercase chars until word ends
-      while buf[wend] > ' ':
-        if buf[wend] in {'A'..'Z'}:
-          buf[wend] = chr(ord(buf[wend]) + (ord('a') - ord('A')))
-        wend.inc()
-      if wend > lastChar:
-        break
-      # Increment counter
-      words.inc(buf[wstart..<wend])
-      wstart = wend + 1
-    
-    # Copy unaccounted partial to the start of a buffer
-    if wstart < totalBytes:
-      offset = totalBytes - wstart
-      moveMem(buf[0].addr(), buf[wstart].addr(), offset)
-    else:
-      offset = 0
+    var 
+      inside = false
+      wstart, wend = 0
+    while wstart <= lastChar:
+      if inside:
+        if buf[wend].isWhitespace: # leaving word
+          inside = false
+          wordBuf.copy(buf{wstart..<wend})
+          words.inc(wordBuf)
+          wstart = wend + 1
+          continue
+        else: # traversing a word
+          if buf[wend] in {'A'..'Z'}:
+            buf[wend] = chr(ord(buf[wend]) + (ord('a') - ord('A')))
+          wend.inc()
+      else:
+        if buf[wstart].isWhitespace: # skipping whitespace
+          wstart.inc()
+          continue
+        else: # entering a word
+          inside = true
+          wend = wstart
+      
+    if offset > 0:
+      # if offset is positive, overflow is impossible
+      moveMem(buf[0].addr(), buf[lastChar + 1].addr(), offset)
 
   words.sort(SortOrder.Descending)
   for (word, count) in words.pairs():
