352fa1d2b6febaed047c9aa3462f7c8317eec06a
ZoomRmc <zoomrmc@gmail.com>
2021-04-05 17:55:46 +0300

    Hashing on-the-go via modifying tables hack
    Since we traverse words char by char it's natural to hash as we go, and
    avoid additional scanning of the word on table insertion. However,
    standard library doesn't provide any way of passing a precalculated
    hash with the item. We can work around this by including whole `tables`
    module inside our source code thus gaining access to all private procs
    and fields. Then it's just a case of overloading a couple of procs.
    
    Nasty hack, ~7% speed gain.

--- optimized.nim
+++ optimized.nim
@@ -1,13 +1,42 @@
-from tables import initCountTable, inc, sort, pairs
+#from tables import initCountTable, inc, sort, pairs
 from algorithm import SortOrder, sort
-from hashes import Hash, `!$`, `!&`
+from hashes import Hash
+
+# Tables hacks
+include tables
+
+proc rawGet[A](t: CountTable[A], key: A, hash: Hash): int =
+  if t.data.len == 0:
+    return -1
+  var h: Hash = hash and high(t.data) # start with real hash value
+  while t.data[h].val != 0:
+    if t.data[h].key == key: return h
+    h = nextTry(h, high(t.data))
+  result = -1 - h # < 0 => MISSING; insert idx = -1 - result
+
+proc inc*[A](t: var CountTable[A], key: A, hash: Hash) =
+  var index = rawGet(t, key, hash)
+  if index >= 0:
+    inc(t.data[index].val)
+    if t.data[index].val == 0:
+      delImplIdx(t, index, cntMakeEmpty, cntCellEmpty, cntCellHash)
+  else:
+    if t.dataLen == 0: initImpl(t, defaultInitialSize)
+    if mustRehash(t): enlarge(t)
+    ctRawInsert(t, t.data, key, 1)
+    inc(t.counter)
+# end of table hacks
+
+const 
+  FNVp = 0x100000001b3'u64
+  FNVm = 0x84222325'u64
 
 proc hash*(x: string): Hash {.inline.} =
   # FNV-1a 64
-  var h = 0x100000001b3'u64
+  var h = FNVp 
   for c in x:
     h = h xor cast[uint64](c)
-    h *= 0x84222325'u64
+    h *= FNVm
   return cast[Hash](h)
 
 # Workaround for superfluous copying on table insertion by @alaviss
@@ -34,6 +63,7 @@ proc main() =
     buf = newString(bufLen)
     offset = 0
     wordBuf: string
+    hash64 = FNVp
 
   while true:
     # Fill buffer starting from offset (remainder from prev. iteration)
@@ -56,12 +86,13 @@ proc main() =
         if buf[wend].isWhitespace(): # leaving word
           inside = false
           wordBuf.copy(buf{wstart..<wend})
-          words.inc(wordBuf)
+          words.inc(wordBuf, hash = cast[Hash](hash64))
           wstart = wend + 1
           continue
         else: # traversing a word
           if buf[wend] in {'A'..'Z'}:
             buf[wend] = chr(ord(buf[wend]) + (ord('a') - ord('A')))
+          hash64 = (hash64 xor cast[uint64](buf[wend])) * FNVm # hash as we go
           wend.inc()
       else:
         if buf[wstart].isWhitespace(): # skipping whitespace
@@ -69,6 +100,7 @@ proc main() =
           continue
         else: # entering a word
           inside = true
+          hash64 = FNVp # init hash for the new word
           wend = wstart
       
     if offset > 0:
